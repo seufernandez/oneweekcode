557ab5bb6e67948de0f37af652b94e7712fcb30b
Gabriel Fernandes <fernandez.g1337@gmail.com>
2021-03-02 13:54:53 -0300

    nodemon

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+//importar dependencia
+const express = require('express');
+
+//iniciando o express
+const server = express()
+
+//criar uma rota
+server.get('/', () =>{
+    
+})
+
+//ligar o servidor
+server.listen(5500)
